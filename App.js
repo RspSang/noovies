70979c819e599a6c363386e1ff5ab3e8c0b6ac55
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-11-16 13:46:24 +0900

    Install expo-splash-screen

--- App.js
+++ App.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return null;
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync(Ionicons.font);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+  return (
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      onLayout={onLayoutRootView}
+    >
+      <Text>SplashScreen Demo! 👋</Text>
+    </View>
+  );
 }
