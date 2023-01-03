e03bcfa94ce62bed6e0898da725153b9b73aeacb
DeepDive <71082215+RspSang@users.noreply.github.com>
2023-01-03 16:23:43 +0900

    Add Movies Screen

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,5 +1,126 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
+import Swiper from "react-native-swiper";
+import styled from "styled-components/native";
+import { makeImgPath } from "../utils";
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => null;
+const API_KEY = "63499326424116264172142ac5886cd9";
+
+const Container = styled.ScrollView``;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  width: 60%;
+`;
+
+const Overview = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+
+const Votes = styled(Overview)`
+  font-size: 12px;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const isDark = useColorScheme() === "dark";
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=JP`
+      )
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
+    <Container>
+      <Swiper
+        horizontal
+        showsButtons={false}
+        autoplay
+        autoplayTimeout={3.5}
+        showsPagination={false}
+        loop
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ url: makeImgPath(movie.backdrop_path) }}
+              blurRadius={10}
+              resizeMode="cover"
+            />
+            <Wrapper>
+              <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+              <Column>
+                <Title isDark={isDark}>{movie.original_title}</Title>
+                {movie.vote_average > 0 ? (
+                  <Votes isDark={isDark}>⭐️ {movie.vote_average}/10</Votes>
+                ) : null}
+                <Overview isDark={isDark}>
+                  {movie.overview.slice(0, 100)}...
+                </Overview>
+              </Column>
+            </Wrapper>
+          </View>
+        ))}
+      </Swiper>
+    </Container>
+  );
+};
+
 export default Movies;
